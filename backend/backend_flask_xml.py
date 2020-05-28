1cd0e0dc09d056aae67b5b2b72afe73f4c6fb11b
sanjit1995 <sonu.sanjit@gmail.com>
2020-05-28 21:59:13 +0530

    8th Draft

--- backend/backend_flask_xml.py
+++ backend/backend_flask_xml.py
@@ -1,31 +1,35 @@
+# Import Libraries
 from flask import Flask, request, jsonify
 import sqlite3 as sql
 import xml.etree.ElementTree as et
 from plotter import drawPlot
 import pandas as pd
 
+# Define app variable
 app = Flask(__name__)
 
+# Define function for /result route
 @app.route('/result', methods=['POST'])
 def result():
+    # Extract the <data> node from the XML
     data = et.fromstring(request.data.decode("utf-8"))
     data = et.ElementTree(data)
     root = data.getroot()
     latitude = ""
     longitude = ""
     height = ""
+    # Loop over the child nodes of <data>
     for data in root:
         for nodes in data:
-            # print(nodes)
             if nodes.tag == "latitude":
                 latitude = nodes.text
             if nodes.tag == "longitude":
                 longitude = nodes.text
             if nodes.tag == "height":
                 height = nodes.text
+        # Open SQL connection to .db file to insert data
         with sql.connect("mySqlite.db") as con:
             cur = con.cursor()
-            # print(str(row[0]), str(row[1]), row[2])
             cur.execute("INSERT INTO location_sensor_data (latitude,longitude,height) VALUES (?,?,?)",
                         (latitude, longitude, height))
             con.commit()
@@ -36,26 +40,30 @@ def result():
             data["latitude"] = pd.to_numeric(data["latitude"], downcast="float")
             data["longitude"] = pd.to_numeric(data["longitude"], downcast="float")
             data["height"] = pd.to_numeric(data["height"], downcast="float")
-            # data.to_csv(path_or_buf=r"c:\react_js_app\public\lat-lon.csv", sep=",", na_rep="NA")
+    # Return a successful Response XML
     return "<?xml version='1.0' encoding='UTF-8'?><sensor_data><status_code>200</status_code><status>OK</status><message>Data has been successfully stored</message></sensor_data>"
 
-
+# Define function for /clear route
 @app.route('/clear', methods=['POST'])
 def clear():
+    # Open SQL connection to .db file to delete data
     with sql.connect("mySqlite.db") as con:
         cur = con.cursor()
         cur.execute("delete from location_sensor_data")
         con.commit()
+    # Return a successful Response XML
     return "<?xml version='1.0' encoding='UTF-8'?><sensor_data><status_code>200</status_code><status>OK</status><message>Data has been cleared</message></sensor_data>"
 
-
+# Define function for /plotImage route
 @app.route('/plotImage', methods=['GET'])
 def plotImage():
+    # Call drawPlot() of plotter file
     imgData = drawPlot()
     if imgData == "NA":
         statusCode = 500
     else:
         statusCode = 200
+    # Return the Response XML
     return "<?xml version='1.0' encoding='UTF-8'?><sensor_data><status_code>" + str(statusCode) + "</status_code><status>OK</status><image_data>" + imgData + "</image_data></sensor_data>"
 
 
--- backend/plotter.py
+++ backend/plotter.py
@@ -1,3 +1,4 @@
+# Import Libraries
 import os
 
 os.environ['PROJ_LIB'] = r'c:\ProgramData\Anaconda3\pkgs\proj4-5.2.0-ha925a31_1\Library\share'
@@ -8,26 +9,30 @@ import io
 import base64
 import sqlite3 as sql
 
+# Define function to draw plot
 def drawPlot():
+    # Open SQL connection to .db file to fetch data
     with sql.connect("mySqlite.db") as con:
         cur = con.cursor()
         cur.execute("select * from location_sensor_data")
         tdata = cur.fetchall()
         data = pd.DataFrame(tdata)
+        # If there is no data in the table, return "NA"
         if len(data) == 0:
-            return "NO DATA AVAILABLE"
+            return "NA"
         data.columns = ['latitude', 'longitude', 'height']
         data["latitude"] = pd.to_numeric(data["latitude"], downcast="float")
         data["longitude"] = pd.to_numeric(data["longitude"], downcast="float")
         data["height"] = pd.to_numeric(data["height"], downcast="float")
-        #print(data)
     df = data
 
+    # Get all latitude and longitude values
     lats = df['latitude'].values
     lons = df['longitude'].values
 
     fig, ax = plt.subplots()
 
+    # Define a Basemap object
     m = Basemap(projection='mill',
                 llcrnrlat=-90,
                 urcrnrlat=90,
@@ -35,21 +40,23 @@ def drawPlot():
                 urcrnrlon=180,
                 ax=ax)
 
-    #m.drawcoastlines()
+    # Draw backgrounds for plot
     m.drawcountries()
-    #m.drawstates()
     m.drawmapboundary(fill_color='aqua')
     m.fillcontinents(color='lightgreen', lake_color='aqua')
-    # convert lat and lon to map projection coordinates
+
+    # Convert lat and lon to map projection coordinates
     lons, lats = m(lons, lats)
 
+    # Plot the latitude and longitude on the map and denote the height at the point
     m.plot(lons, lats, marker='.', markersize=5, linewidth=1, color='r')
     heights = df['height']
     for i in range(0, len(heights)):
         ax.annotate(str(heights[i]) + "m", (lons[i], lats[i]), xytext=(0.01, 0.01), textcoords='offset points')
+    # Convert the image to base64 format
     buf = io.BytesIO()
     plt.savefig(buf, format='png', dpi=1280)
     buf.seek(0)
     img_base64 = base64.b64encode(buf.read())
-    #return jsonify({'status': str(img_base64)})
+    # Return the base64 converted string
     return str(img_base64)
